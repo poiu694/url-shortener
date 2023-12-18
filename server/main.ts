fd13842affedb1d879408686e0bc4debe16e4311
Lee <43488305+poiu694@users.noreply.github.com>
2023-12-18 13:38:14 +0900

    feat: cors

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
-import type { Application } from 'express';
+import type { Request, Response, Application, NextFunction } from 'express';
+
+import { base62 } from './utils/base62';
 
 const PORT = 8080;
 
@@ -9,6 +11,12 @@ export default class App {
 
   constructor() {
     this.app = express();
+
+    this.config();
+  }
+
+  private config() {
+    this.app.use(cors);
   }
 
   public listen() {
@@ -16,4 +24,24 @@ export default class App {
       console.log('server start');
     });
   }
+}
+
+const ORIGIN = 'http://localhost:5173';
+const OPTIONS_SUCCESS_STATUS = 204;
+function cors(req: Request, res: Response, next: NextFunction) {
+  const method = req.method && req.method.toUpperCase && req.method.toUpperCase();
+
+  res.header('Access-Control-Allow-Origin', ORIGIN);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+  if (method === 'OPTIONS') {
+    res.statusCode = OPTIONS_SUCCESS_STATUS;
+    res.setHeader('Content-Length', 0);
+    res.end();
+    return;
+  }
+  next();
 }
